9299bc02f65317c1aa68e58b08efeb87200f1927
Marco <antoni90211@gmail.com>
2025-02-18 20:37:05 -0600

    Forzando actualización total

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,10 +5,10 @@
  */
 function calculateRentalCost(days) {
   // write code here
-let day =1;
-  let dayRental = 40;
-let sevenDaysOff= 50;
-let threeDaysOff= 20;
+
+  const dayRental = 40;
+const sevenDaysOff= 50;
+const threeDaysOff= 20;
 
 if(days === 1 ){
 
@@ -20,7 +20,7 @@ if(days === 1 ){
  let totalSeven = days * dayRental - sevenDaysOff
 
 return totalSeven
-}if(days >=3 && day <= 6){
+}if(days >=3 && days <= 6){
 
   let totalThree = days *dayRental - threeDaysOff
   return totalThree
