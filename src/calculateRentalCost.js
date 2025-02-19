b42ae0f988b6423ef17d22e15b77e12d31d36f31
Marco <antoni90211@gmail.com>
2025-02-18 20:37:50 -0600

    Forzando actualización total

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -12,17 +12,17 @@ const threeDaysOff= 20;
 
 if(days === 1 ){
 
-  let totalOneDay= days * 40
+  const totalOneDay= days * 40
 
   return totalOneDay
 } if(days >= 7){
 
- let totalSeven = days * dayRental - sevenDaysOff
+ const totalSeven = days * dayRental - sevenDaysOff
 
 return totalSeven
 }if(days >=3 && days <= 6){
 
-  let totalThree = days *dayRental - threeDaysOff
+  const totalThree = days *dayRental - threeDaysOff
   return totalThree
 }
 
