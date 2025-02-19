743b9622531b821fea8b3ee87616976b127134d8
Marco <antoni90211@gmail.com>
2025-02-18 19:47:08 -0600

    Subiendo cambios

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -15,13 +15,11 @@ if(days === 1 ){
   let totalOneDay= days * 40
 
   return totalOneDay
-
 }else if(days >= 7){
 
  let totalSeven = days * dayRental - sevenDaysOff
 
 return totalSeven
-
 }else if(days >=3 && day <= 6){
 
   let totalThree = days *dayRental - threeDaysOff
