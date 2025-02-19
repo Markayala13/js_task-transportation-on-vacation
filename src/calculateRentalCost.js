0a52ef0b4a7db5aa8a0f555a96c91d2744c95b05
Marco <antoni90211@gmail.com>
2025-02-18 18:05:02 -0600

    Forzando ejecución de GitHub Actions

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,36 @@
  */
 function calculateRentalCost(days) {
   // write code here
+let day =1;
+  let dayRental = 40;
+let sevenDaysOff= 50;
+let threeDaysOff= 20;
+
+if(days === 1 ){
+
+  let totalOneDay= days * 40
+
+  return totalOneDay
+
+}else if(days >= 7){
+
+ let totalSeven = days * dayRental - sevenDaysOff
+
+return totalSeven
+
+}else if(days >=3 && day <= 6){
+
+  let totalThree = days *dayRental - threeDaysOff
+  
+  return totalThree
 }
 
-module.exports = calculateRentalCost;
+
+
+
+
+}
+
+console.log(calculateRentalCost(7))
+
+//module.exports = calculateRentalCost;
